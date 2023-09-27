ebfa3e47a9381a2fee234df5f86e703bc937f86d
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-09-27 18:55:45 +0200

    Change text

--- controllers/postController.js
+++ controllers/postController.js
@@ -7,7 +7,7 @@ const Post = require("../models/Post")
 exports.get_posts =async(req,res,next)=>{
     const allPosts = await Post.find().exec()
     const count = await Post.count()
-    if(count==0) return res.status(404).json("We dont have any post!")
+    if(count==0) return res.status(404).json("We dont have any post")
      return res.status(200).json(allPosts)
 
 }
